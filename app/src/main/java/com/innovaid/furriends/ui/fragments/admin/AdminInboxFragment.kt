f070d180589877e07a6efbd3620adccfda70af1e
Paradosseus <colladofrancis524@gmail.com>
2022-11-24 06:57:19 +0800

    Added other features and placeholders

--- app/src/main/java/com/innovaid/furriends/ui/fragments/admin/AdminInboxFragment.kt
+++ app/src/main/java/com/innovaid/furriends/ui/fragments/admin/AdminInboxFragment.kt
@@ -1,28 +1,49 @@
 package com.innovaid.furriends.ui.fragments.admin
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.TextView
 import com.innovaid.furriends.R
+import com.innovaid.furriends.ui.activities.MessageActivity
 
 
 class AdminInboxFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
+        setHasOptionsMenu(true)
 
+
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_inbox, container, false)
+        val root = inflater.inflate(R.layout.fragment_admin_inbox, container, false)
+        val textView: TextView = root.findViewById(R.id.tvAdminInbox)
+        textView.text = "You have no messages"
+        return root
     }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_create_message_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        when (id) {
+            R.id.actionCreateNewMessage -> {
+                startActivity(Intent(activity, MessageActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
 }
--- app/src/main/java/com/innovaid/furriends/ui/fragments/user/UserInboxFragment.kt
+++ app/src/main/java/com/innovaid/furriends/ui/fragments/user/UserInboxFragment.kt
@@ -1,18 +1,26 @@
 package com.innovaid.furriends.ui.fragments.user
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 //import androidx.lifecycle.ViewModelProviders
 import com.innovaid.furriends.R
+import com.innovaid.furriends.ui.activities.MessageActivity
+import com.innovaid.furriends.ui.activities.user.AddUserPetProfileActivity
 
 
 class UserInboxFragment : Fragment() {
 
     //private lateinit var inboxViewModel: InboxViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+
+
+    }
 
 
     override fun onCreateView(
@@ -26,6 +34,23 @@ class UserInboxFragment : Fragment() {
 
             textView.text = "You have no messages"
         return root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_create_message_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        when (id) {
+            R.id.actionCreateNewMessage -> {
+                startActivity(Intent(activity, MessageActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
